be5195cba5dcd6b81dc25da9e32c9bbc6e92cf3a
Eric McCoy <divereric0@gmail.com>
2024-12-10 15:06:23 -0500

    added API call to newsapi.com

--- NewsAPI/ContentView.swift
+++ NewsAPI/ContentView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var json = "Loading"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Goodbye, World!")
+            
+            Text(json)
+        }
+        .task {
+            let url = URL(string: "https://newsapi.org/v2/everything?q=tesla&from=2024-11-10&sortBy=publishedAt&apiKey=c371d63994084e1a967dea1634098b64")!
+
+               let (data, _) = try! await URLSession.shared.data(from: url)
+
+               json = String(decoding: data, as: UTF8.self)
         }
         .padding()
     }
